e742494c06af6638db1ac3300eda53b631c352ad
toshitalol <dogratoshita24@gmail.com>
2026-02-11 02:35:56 +0530

    Update 1.1.1.ino

--- 1.1.1.ino
+++ 1.1.1.ino
@@ -1,20 +1,15 @@
-const int irAnalogPin = A0;   // A0 connected to sensor A0
-int sensorValue = 0;          // Variable to store reading
+const int irAnalogPin = A0;  
+int sensorValue = 0;       
 
 void setup() {
-  Serial.begin(9600);         // Start Serial Monitor
+  Serial.begin(9600);        
   pinMode(irAnalogPin, INPUT);
 }
 
 void loop() {
-  
-  // Read analog value (0–1023)
+=
   sensorValue = analogRead(irAnalogPin);
-
-  // Print value to Serial Monitor
   Serial.print("IR Reflectivity Value: ");
   Serial.println(sensorValue);
-
-  // Small delay for stability
   delay(500);
 }
